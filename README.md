88efdf11b2adfb3c5043dc9738192f909b8120d8
takayasu.nasu <taka.beckham@gmail.com>
2021-04-09 00:02:20 -0700

    first commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+It is built with Typescript and Gatsby.
---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import type { FC } from 'react'
+import { Link } from 'gatsby'
+
+const Page: FC = () => (
+  <div>
+    <h1>About</h1>
+    <Link to="/">Home</Link>
+  </div>
+)
+export default Page
